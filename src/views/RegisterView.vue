b855c2eeee8b94072a7866281c99364e47c70ddc
Muhammad Rizwan <xrizwanr@gmail.com>
2022-11-02 21:14:18 +0500

    added RegisterView and deleted AboutView

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,47 @@
+<template>
+  <main>
+    <section class="column center">
+      <Input placeholder="Enter Username"/>
+      <Input placeholder="Enter Password"/>
+      <Button text="Sign Up!" class="btn"/>
+    </section>
+  </main>
+</template>
+
+<script lang="ts">
+import Input from '../components/Input.vue';
+import Button from '../components/Button.vue';
+
+export default {
+  name: 'RegisterView',
+  components: {Input, Button},
+}
+</script>
+
+<style>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 150px;
+}
+input {
+  margin-bottom: 10px;
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+}
+.column {
+  display: flex;
+  flex-direction: column;
+}
+.center {
+  align-items: center;
+}
+.btn {
+  width: 80px;
+  height: 30px;
+}
+</style>
