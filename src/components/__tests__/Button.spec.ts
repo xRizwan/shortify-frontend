72f76731e5092c3085bc2976308086fb7711bf36
Muhammad Rizwan <xrizwanr@gmail.com>
2022-11-04 15:17:38 +0500

    updated button props

--- src/components/__tests__/Button.spec.ts
+++ src/components/__tests__/Button.spec.ts
@@ -1,11 +1,35 @@
-import { describe, it, expect } from "vitest";
+import {
+  describe,
+  it,
+  expect,
+  MockedFunction,
+  vi,
+  beforeEach,
+  beforeAll,
+  Mock,
+} from "vitest";
 
-import { mount } from "@vue/test-utils";
+import { mount, VueWrapper } from "@vue/test-utils";
 import Button from "../Button.vue";
 
-describe("Button", () => {
+describe("Button", async () => {
+  let wrapper: VueWrapper<any>;
+  let fn: Mock;
+  beforeAll(() => {
+    fn = vi.fn();
+    wrapper = mount(Button, {
+      props: { text: "This is a button!", "onOn-click": fn },
+    });
+  });
+
   it("renders properly", () => {
-    const wrapper = mount(Button, { props: { text: "This is a button!" } });
     expect(wrapper.text()).toContain("This is a button!");
+  });
+
+  it("triggers function on click", async () => {
+    await wrapper.trigger("click");
+    await wrapper.trigger("click");
+
+    expect(fn).toBeCalledTimes(2);
   });
 });
